d60641a09a891e465bbc542abdf88cf4058a91f4
MubashirAlam466 <mubashiralam466@gmail.com>
2023-07-25 02:08:47 +0500

    I Am Working On PHP Variable

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
 <?php
-echo "Mubashir Alam";
+
+echo "<br>";
+
+$Name = "Mubashir Alam";
+$Fname = "Muhammad Alam";
+$Contact = "03460315462";
+$Address = "North Nazimabad Karachi";
+echo "My Name Is : $Name <br> My Father Name IS : $Fname <br>
+      Contact No : $Contact <br> Address : $Address  <br>";
+
+     echo "<br>";
+     echo "<br>";
+
+$num = 550;
+$num1 = 550;
+$Sum = $num+$num1;
+$Subtraction = $num-$num1;
+$Multiple = $num*$num1;
+$Division = $num/$num1;
+echo "A Value Of Is Sum : $Sum <br> A Value Of Is Subtraction : $Subtraction <br>
+      A Value Of Is Multiple : $Multiple <br> A Value Of Is Division : $Division <br> ";
+
+    //   A variable in PHP is the name of the memory location that holds data. In PHP,
+    //   a variable is declared using the $ sign followed by the variable name. 
+    //   The main way to store information in the middle of a PHP program is by using a variable.
+    //   I Am Working On PHP Variable Date Is : 25/07/2023 And Time Is : 2:00 Am  And Day Is Tuesday !!!
+
+?>
