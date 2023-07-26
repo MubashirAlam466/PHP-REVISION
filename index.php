00a700af3950368b464428adf9946232d695e5c5
MubashirAlam466 <mubashiralam466@gmail.com>
2023-07-27 02:46:09 +0500

    PHP-REVISION

--- index.php
+++ index.php
@@ -26,6 +26,113 @@ echo "A Value Of Is Sum : $Sum <br> A Value Of Is Subtraction : $Subtraction <br
     //   The main way to store information in the middle of a PHP program is by using a variable.
     //   I Am Working On PHP Variable Date Is : 25/07/2023 And Time Is : 2:00 Am  And Day Is Tuesday !!!
 
+    echo "<br>";
+    echo "<br>";
+
+$x = 245;
+$y = 455;
+echo "This Operator  Is Sum : " . $x + $y . "<br>" ;
+echo "This Operator  Is Subtraction : " . $x - $y . "<br>" ;
+echo "This Operator  Is Multiple : " . $x * $y . "<br>" ;
+echo "This Operator  Is Division : " . $x / $y . "<br>" ;
+
+    //  PHP Operator is a symbol i.e used to perform operations on operands. In simple words,
+    //  operators are used to perform operations on variables or values. For example: $num=10+20;
+    //  + is the operator and 10,20 are operands.
+    //   I Am Working On PHP Variable Date Is : 25/07/2023 And Time Is : 2:00 Am  And Day Is Tuesday !!!
+
+    echo "<br>";
+    echo "<br>";
+
+$age = 18;
+
+if($age >17){
+    echo "Your Are Not Eligible Driving Car Sorry";
+}
+elseif($age >15){
+    echo "Your Are Not Eligible Driving Car Sorry";
+}
+elseif($age >13){
+    echo "Your Are Not Eligible Driving Car Sorry";
+}
+elseif($age >12){
+    echo "Your Are Not Eligible Driving Car Sorry";
+}
+else{
+    echo "Your Are Eligible Driving Car";
+}
+
+    echo "<br>";
+    // echo "<br>";
+
+if($age <25){
+    echo "Your Are Eligible Driving Car";
+}
+elseif($age <22){
+    echo "Your Are Eligible Driving Car";
+}
+elseif($age <20){
+    echo "Your Are Eligible Driving Car";
+}
+elseif($age <18){
+    echo "Your Are Eligible Driving Car";
+}
+else{
+    echo "Your Are Not Eligible Driving Car Sorry";
+}
+
+    // It executes one block of code if the specified condition is true and another block of code
+    // if the condition is false. Syntax. if(condition){ code to be executed if true. }else{
+
+        echo "<br>";
+        echo "<br>";
+
+$Marksheet = 33;
+
+if($Marksheet >32){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet >30){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet >25){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet >20){
+    echo "You Are  Semester Is Pass Congratulation";
+}else{
+    echo "You Are  Semester Is Failed ";
+}
+
+        echo "<br>";
+        // echo "<br>";
+
+if($Marksheet <32){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet <30){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet <25){
+    echo "You Are  Semester Is Pass Congratulation";
+}
+elseif($Marksheet <20){
+    echo "You Are  Semester Is Pass Congratulation";
+}else{
+    echo "You Are  Semester Is Failed ";
+}
+
+    //   It executes one block of code if the specified condition is true and another block of code
+    //   if the condition is false. Syntax. if(condition){ code to be executed if true. }else{
+    //   I Am Working On PHP if & elseif & else Statments Date Is : 27/07/2023 And Time Is : 2:35 Am  And Day Is Thursday !!!
+
+        echo "<br>";
+        echo "<br>";
+
+
+
+
+
 ?>
 
 
