5c3d70e4c8341778f33c2bd5754b2b539e43e202
MubashirAlam466 <mubashiralam466@gmail.com>
2023-07-26 22:31:47 +0500

    PHP-REVISION

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ echo "A Value Of Is Sum : $Sum <br> A Value Of Is Subtraction : $Subtraction <br
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP REVISION</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
